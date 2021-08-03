56827947597c8c6ed9230c89ffaccb69fa25285e
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-08-03 10:18:57 +0530

    setted up firebase config - 🔥

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,25 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firestore/firestore'
+
 
 const firebaseConfig = {
-  apiKey:           process.env.React_App_APIKEY,
-  authDomain:       process.env.React_App_AUTHDOMAIN,
-  projectId:        process.env.React_App_PROJECTID,
-  storageBucket:    process.env.React_App_STORAGEBUCKET,
-  messagingSenderId:process.env.React_App_MESSAGINGSENDERID,
-  appId:            process.env.React_App_APIID
+  apiKey: process.env.React_App_APIKEY,
+  authDomain: process.env.React_App_AUTHDOMAIN,
+  projectId: process.env.React_App_PROJECTID,
+  storageBucket: process.env.React_App_STORAGEBUCKET,
+  messagingSenderId: process.env.React_App_MESSAGINGSENDERID,
+  appId: process.env.React_App_APIID
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
 
-export const db = firebaseApp.firestore()
-export const storage = firebaseApp.storage()
-export const auth = firebaseApp.auth()
-export const provider = new firebase.auth.GoogleAuthProvider();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+export const provider = new firebase.auth.GoogleAuthProvider()
+export const serverTimeStamp = firebase.firestore.FieldValue.serverTimestamp
 
